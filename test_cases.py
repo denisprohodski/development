83bfcc0d3221191ae5b5faff20ee0a27f50cba8c
denisprohodski <denisprohodski@gmail.com>
2023-11-20 18:42:28 +0400

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.fixture(scope='session')  # Please verify database credentials
 def db_conn():
     conn = pymssql.connect(server="host.docker.internal",
-                           port=51829,
+                           port=8080,
                            user='DZIANIS_PRAKHODSKI',
                            password='19eo53Q11111',
                            database='TRN')
