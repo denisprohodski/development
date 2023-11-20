eda96fa48af8766a2ee7b4891280e3137e710d73
denisprohodski <151527979+denisprohodski@users.noreply.github.com>
2023-11-21 01:25:30 +0400

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -38,7 +38,7 @@ pipeline {
         stage('Push to Release Branch') {
             steps {
                 dir('git_repo') {
-                    sh "git checkout -b release" // Create and switch to 'release' branch
+                    sh "git checkout release" // Create and switch to 'release' branch
                     sh "git add ." // Stage changes
                     sh "git commit -m 'Add test results'" // Commit changes
                     sh "git push origin release" // Push changes to 'release' branch
