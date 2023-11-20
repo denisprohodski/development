fccd3c2a1fdb18274c23a692125cc44239fe2ed1
denisprohodski <denisprohodski@gmail.com>
2023-11-20 17:02:47 +0400

    Create Jenkins.groovy

---
+++ Jenkins.groovy
@@ -0,0 +1,48 @@
+pipeline {
+    agent any
+    stages {
+        stage('Download Repository Files') {
+            steps {
+                // clone the repository to a local folder
+                dir('git_repo') {
+                    git(url: 'git@github.com:denisprohodski/development.git', branch: 'development')
+                }
+            }
+        }
+ 
+        stage('Checkout') {
+            steps {
+                // Check out source code
+                checkout scm
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                // Install required dependencies
+                sh '''
+                    python3 -m venv env
+                    . env/bin/activate
+                    python3 -m pip install -r requirements.txt
+                '''
+            }
+        }
+        stage('Run Tests') {
+            steps {
+                // Run your pytest tests
+                sh '''	
+                    . env/bin/activate
+                    pytest --junitxml=results.xml test_cases.py
+                '''
+            }
+        }
+        stage('Push to Release Branch') {
+            steps {
+                dir('git_repo') {
+                    sh '''
+                        git push origin main:release
+                    '''
+                }
+            }
+        }
+    }
+}
